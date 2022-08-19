9971a1a468620573138e352503827afa4a30027f
Gomeschian <97569105+Gomeschian@users.noreply.github.com>
2022-08-18 21:00:11 -0700

    Set up list of keys.

---
+++ trashville.js
@@ -0,0 +1,18 @@
+const num_of_keys = 12;
+const max_chord_progression_length = 8;
+const musical_key_list = [
+  "A",
+  "Bb",
+  "B",
+  "C",
+  "C#",
+  "D",
+  "Eb",
+  "E",
+  "F",
+  "F#",
+  "G",
+  "Ab",
+];
+
+let app = document.querySelector(".app-container");
