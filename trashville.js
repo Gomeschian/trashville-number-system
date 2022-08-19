4795e05347e2abcee98d898e6e8422ef57ca241e
Gomeschian <97569105+Gomeschian@users.noreply.github.com>
2022-08-18 21:28:07 -0700

    Add placeholder button to generate song. Create functions to generate key and chord progression length.

--- trashville.js
+++ trashville.js
@@ -14,5 +14,25 @@ const musical_key_list = [
   "G",
   "Ab",
 ];
+let key = "";
+let chord_progression_length = 0;
+
+function getRandomIntInclusive(min, max) { //Function credit to MDN Web Docs
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+  }
+
+function choose_key()
+{
+    
+    let key_number = getRandomIntInclusive(1, num_of_keys)
+    key = musical_key_list[key_number - 1];
+}
+function choose_chord_progression_length()
+{
+    chord_progression_length = getRandomIntInclusive(1, max_chord_progression_length);
+}
+
 
 let app = document.querySelector(".app-container");
