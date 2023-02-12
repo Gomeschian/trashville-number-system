6ab9e71e302fb7d6334f08544bd6ace22a3382b7
Gomeschian <97569105+Gomeschian@users.noreply.github.com>
2023-02-11 17:06:06 -0800

    Add minor keys to key list.

--- trashville.js
+++ trashville.js
@@ -13,26 +13,38 @@ const musical_key_list = [
   "F#",
   "G",
   "Ab",
+  "Am",
+  "Bbm",
+  "Bm",
+  "Cm",
+  "C#m",
+  "Dm",
+  "Ebm",
+  "Em",
+  "Fm",
+  "F#m",
+  "Gm",
+  "Abm",
 ];
 let key = "";
 let chord_progression_length = 0;
 
-function getRandomIntInclusive(min, max) { //Function credit to MDN Web Docs
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); 
-  }
-
-function choose_key()
-{
-    
-    let key_number = getRandomIntInclusive(1, num_of_keys)
-    key = musical_key_list[key_number - 1];
-}
-function choose_chord_progression_length()
-{
-    chord_progression_length = getRandomIntInclusive(1, max_chord_progression_length);
+function getRandomIntInclusive(min, max) {
+  //Function credit to MDN Web Docs
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function choose_key() {
+  let key_number = getRandomIntInclusive(1, num_of_keys);
+  key = musical_key_list[key_number - 1];
+}
+function choose_chord_progression_length() {
+  chord_progression_length = getRandomIntInclusive(
+    1,
+    max_chord_progression_length
+  );
+}
 
 let app = document.querySelector(".app-container");
